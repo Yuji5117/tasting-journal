79f376e123d91c287717344e8db79701b02b8838
Yuji Nakamura <ny5117@icloud.com>
2026-06-07 15:48:19 +1000

    Add category constatnt

---
+++ features/tasting-notes/constants/categories.ts
@@ -0,0 +1,7 @@
+export const categories = [
+  { label: "All", value: "all" },
+  { label: "Wine", value: "wine" },
+  { label: "Sake", value: "sake" },
+  { label: "Beer", value: "beer" },
+  { label: "Shochu", value: "shochu" },
+] as const;
